29c162c9ef2de2a4cf0f9a4b7a5636448e5e07fd
YUHANG ZHAO <zhaoy36@mcmaster.ca>
2019-10-25 16:41:22 -0400

    Created methods for "Get" posting by Id and "Delete" posting by Id.

--- ITJobWebsite-SpringBoot-MongoDB/src/main/java/org/yuhangz/ITJobWebsite/controller/JobPostingController.java
+++ ITJobWebsite-SpringBoot-MongoDB/src/main/java/org/yuhangz/ITJobWebsite/controller/JobPostingController.java
@@ -1,9 +1,12 @@
 package org.yuhangz.ITJobWebsite.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,4 +30,14 @@ public class JobPostingController {
     public JobPosting postBook(@RequestBody JobPosting jp) {
         return jpService.createJobPosting(jp);
     }
+    
+	@GetMapping("/{id}")
+	public Optional<JobPosting> getJobPostingById(@PathVariable("id") int id) {
+		return jpService.getJobPostingById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public Optional<JobPosting> deleteJobPostingById(@PathVariable("id") int id) {
+		return jpService.deleteJobPostingById(id);
+	}
 }
--- ITJobWebsite-SpringBoot-MongoDB/src/main/java/org/yuhangz/ITJobWebsite/service/JobPostingService.java
+++ ITJobWebsite-SpringBoot-MongoDB/src/main/java/org/yuhangz/ITJobWebsite/service/JobPostingService.java
@@ -1,6 +1,7 @@
 package org.yuhangz.ITJobWebsite.service;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,16 @@ public class JobPostingService {
 	
 	public JobPosting createJobPosting(JobPosting jp) {        
 		return repo.insert(jp);
-    }
+	}
+			
+	public Optional<JobPosting> getJobPostingById(int id) {
+	    return repo.findById(id);
+	}
+	
+    public Optional<JobPosting> deleteJobPostingById(int id) {
+        Optional<JobPosting> jp = repo.findById(id);
+        jp.ifPresent(j -> repo.delete(j));
+        return jp;
+   }
+    
 }
